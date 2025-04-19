65f7a9d29e0aacccc822ce7cc1b6774ef4f54bf3
Enver Yigitler <enverygtlr@hotmail.com>
2025-04-19 13:35:13 +0300

    Initial commit.

---
+++ src/main/java/com/enverygtlr/dopingcase/DopingCaseApplication.java
@@ -0,0 +1,13 @@
+package com.enverygtlr.dopingcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopingCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DopingCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enverygtlr/dopingcase/DopingCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enverygtlr.dopingcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopingCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
